367169a277aa28918f7be0ddadbef816b6a9df03
TinaLepres <tina.lepres14@gmail.com>
2026-09-23 17:27:57 +0200

    my changes

--- assignment.py
+++ assignment.py
@@ -1,6 +1,11 @@
+#stores a string
 drink = "tea"
+# links numbers to strings
 sales = {"tea": 3, "coffee": 5}
+#number associated to drink
 sold = sales[drink]
+#plus 1 to sold
 sold = sold + 1
+#print
 print(drink)
 print(sold)
